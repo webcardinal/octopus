becdbc25015e9dd39d0545c58badc11ad93fd1ef
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2020-06-01 16:27:36 +0300

    bind-app cmd update: removing -prototype substr from appName

--- scripts/bindApp.js
+++ scripts/bindApp.js
@@ -1,19 +1,20 @@
 function populateActions(actions, solution, app) {
+	let appNameWithoutPrototype = app.replace("-prototype", "");
 	actions.push({
 		"type": "copy",
 		"src": `./${app}/seed`,
-		"target": `./web-server/${solution}/apps/${app}/seed`,
+		"target": `./web-server/${solution}/apps/${appNameWithoutPrototype}/seed`,
 		"options": {
 			overwrite: true
 		}
 	});
 	actions.push({
 		"type": "mkdir",
-		"target": `./web-server/${solution}/${app}-template`
+		"target": `./web-server/${solution}/${appNameWithoutPrototype}-template`
 	});
 	actions.push({
 		"type": "execute",
-		"cmd": `echo Use this folder template in order to customize the application instance by adding configuration, pages etc. > ./web-server/${solution}/${app}-template/readme`
+		"cmd": `echo Use this folder template in order to customize the application instance by adding configuration, pages etc. > ./web-server/${solution}/${appNameWithoutPrototype}-template/readme`
 	});
 }
 
